f539173d08799a25ad06a81f924aa37c9cde8d2c
cristian <cristian.h.santos6@aluno.senai.br>
2026-07-30 15:26:45 -0300

    add

--- src/app/lista-produtos/lista-produtos.ts
+++ src/app/lista-produtos/lista-produtos.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
-export class ListaProdutos {}
+
+export class ListaProdutos {
+  id_produto: number = 0;
+  descricao_produto: string = '';
+  valor_unitario: number = 0.0;
+
+
+  addItem() {
+    console.log(this.descricao_produto, '<->', this.valor_unitario)
+  }
+
+}
+
